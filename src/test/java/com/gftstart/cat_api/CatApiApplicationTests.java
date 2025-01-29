47b5412992b45143c9e1e765cfc02d4704925609
nivlekrod <kmrkelvin@hotmail.com>
2025-01-29 09:53:11 -0300

    configuracao do projeto

---
+++ src/test/java/com/gftstart/cat_api/CatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gftstart.cat_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
